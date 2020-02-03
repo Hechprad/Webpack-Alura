2aad8702c1ca120a682cd0050f5efba807e63a03
Jorge Hecherat <hecherat@gmail.com>
2020-02-03 16:06:25 -0300

    Configure module rules to execute loader on js files

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //search for all .js files
+        exclude: /node_modules/, //doesn't look for js files in node_modules folder
+        use: {
+          loader: "babel-loader" //run babel loader on js files
+        }
+      }
+    ]
   }
 };
